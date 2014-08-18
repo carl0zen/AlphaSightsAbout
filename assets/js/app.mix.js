42775d775d09638fcd2554e7bfd77673243bf43f
carlosepp <carlosepp@gmail.com>
2014-08-18 00:04:31 -0500

    starting working on funcionality

---
+++ assets/js/app.mix.js
@@ -0,0 +1,9 @@
+@import 'vendor/jquery.js';
+@import 'vendor/skrollr.js';
+@import 'vendor/foundation/foundation.js';
+
+$(document).ready(function(e){
+	
+	var s = skrollr.init();
+	$(document).foundation();
+});
