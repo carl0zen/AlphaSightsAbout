177bc2c76eec9bb732ae650f0d2949675144710c
carlosepp <carlosepp@gmail.com>
2014-08-21 18:54:37 -0500

    Animating Main Section

--- assets/js/app.mix.js
+++ assets/js/app.mix.js
@@ -2,10 +2,22 @@
 //@import 'vendor/skrollr.js';
 @import 'vendor/foundation/foundation.js';
 @import 'vendor/foundation/foundation.orbit.js';
-
+@import 'vendor/waypoints.js';
 
 $(document).ready(function(e){
-	
+	//Animate from top
+	//Animate from top
+$('.animated').waypoint(function() {
+	$(this).toggleClass($(this).data('animated'));
+},
+{ offset: 70 });
+	/** 
+	//Animate from bottom
+	$('.animated').waypoint(function() {
+		$(this).toggleClass($(this).data('animated'));
+	},
+	{ offset: 'bottom-in-view' });
+	**/
 	//var s = skrollr.init();
 	$(document).foundation();
 });
