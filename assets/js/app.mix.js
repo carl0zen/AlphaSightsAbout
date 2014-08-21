852970658e5ba5570fae7ede667d248b92b8cff1
carlosepp <carlosepp@gmail.com>
2014-08-20 20:53:28 -0500

    Positions CSS Styling done TODO: background and debugging

--- assets/js/app.mix.js
+++ assets/js/app.mix.js
@@ -1,11 +1,11 @@
 @import 'vendor/jquery.js';
-@import 'vendor/skrollr.js';
+//@import 'vendor/skrollr.js';
 @import 'vendor/foundation/foundation.js';
 @import 'vendor/foundation/foundation.orbit.js';
 
 
 $(document).ready(function(e){
 	
-	var s = skrollr.init();
+	//var s = skrollr.init();
 	$(document).foundation();
 });
