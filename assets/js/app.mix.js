2dc4bcc7c39178fc6d56348d59b03aa08a742e82
carlosepp <carlosepp@gmail.com>
2014-08-22 01:11:25 -0500

    JS Navigation Complete TODO: profile modal boxes & responsive menu

--- assets/js/app.mix.js
+++ assets/js/app.mix.js
@@ -5,21 +5,25 @@
 @import 'vendor/waypoints.js';
 
 $(document).ready(function(e){
-	//Animate from top
-	//Animate from top
-var offset;
-$('.animated').waypoint(function() {
-	$(this).toggleClass($(this).data('animated'));
-	
-},
-{ offset: $(window).height()-200 });
-	/** 
-	//Animate from bottom
+
+	var offset;
 	$('.animated').waypoint(function() {
 		$(this).toggleClass($(this).data('animated'));
+		
 	},
-	{ offset: 'bottom-in-view' });
-	**/
-	//var s = skrollr.init();
+	{ offset: $(window).height()-200 });
 	$(document).foundation();
+	//Adding click to scroll functionality
+	$(".scroll").click(function(event){
+         event.preventDefault();
+         //calculate destination place
+         var dest=0;
+         if($(this.hash).offset().top > $(document).height()-$(window).height()){
+              dest=$(document).height()-$(window).height();
+         }else{
+              dest=$(this.hash).offset().top;
+         }
+         //go to destination
+         $('html,body').animate({scrollTop:dest-64}, 1000,'swing');
+     });
 });
